05f270de16d3e94f9e255990215b311d69d0f16f
jbphet <john.blanco@colorado.edu>
2021-12-02 16:37:43 -0700

    added docs, changed stub file name, see https://github.com/phetsims/quake/issues/3

--- js/js.js
+++
@@ -1,1 +0,0 @@
-// Copyright 2021, University of Colorado Boulder
---
+++ js/stub.js
@@ -0,0 +1,9 @@
+// Copyright 2021, University of Colorado Boulder
+
+/**
+ * This is a temporary file that does nothing, and exists as a placeholder so that the lint-everything task will
+ * succeed.
+ *
+ * TODO: This file should be removed once there is other JS code in this repo, see
+ * https://github.com/phetsims/quake/issues/3.
+ */
