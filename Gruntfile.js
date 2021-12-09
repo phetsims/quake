6436dcbccd03fec04b3c1f7ed9a741e656250cb8
jbphet <john.blanco@colorado.edu>
2021-12-09 15:24:26 -0700

    ignore node_modules, use sim lint file, see https://github.com/phetsims/perennial/issues/254

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
-// Copyright 2002-2020, University of Colorado Boulder
+// Copyright 2020, University of Colorado Boulder
 
+/* eslint-env node */
 
 // use chipper's gruntfile
 module.exports = require( '../chipper/js/grunt/Gruntfile.js' );
