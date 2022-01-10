548be7eb6820700ca5faf51ba4263d35ff5a0e42
jbphet <john.blanco@colorado.edu>
2022-01-10 15:37:53 -0700

    updated Gruntfile.js to include lint by default and be more modular

--- js/Gruntfile.js
+++ js/Gruntfile.js
@@ -16,16 +16,13 @@ const CUSTOM_VIBRATION_PLUGIN_NAME = 'cordova-plugin-native-vibration';
 module.exports = grunt => {
   Gruntfile( grunt );
 
+  // Register the task that installs or updates the custom native vibration plugin.
   grunt.registerTask(
-    'build',
-    'Build the Haptics Playground app for all supported platforms',
+    'install-or-update-plugin',
+    'Install or update the custom native vibration plugin',
     () => {
-      let commandResult;
-      grunt.log.writeln( '' );
-
-      // Install or update the custom native vibration plugin.
       grunt.log.writeln( `Checking whether ${CUSTOM_VIBRATION_PLUGIN_NAME} plugin is present...` );
-      commandResult = child_process.execSync( 'cordova plugin list' );
+      let commandResult = child_process.execSync( 'cordova plugin list' );
       if ( commandResult.includes( CUSTOM_VIBRATION_PLUGIN_NAME ) ) {
         grunt.log.writeln( '  Plugin was previously added, updating it...' );
         commandResult = child_process.execSync( 'cordova plugin update cordova-plugin-native-vibration' );
@@ -34,16 +31,23 @@ module.exports = grunt => {
         grunt.log.writeln( '  Plugin not installed, adding it...' );
         commandResult = child_process.execSync( `cordova plugin add ./${CUSTOM_VIBRATION_PLUGIN_NAME}/` );
       }
+      grunt.log.writeln( `${commandResult}` );
       grunt.log.writeln( 'Plugin is installed and current.' );
+    }
+  );
+
+  grunt.registerTask(
+    'build',
+    'Build the Haptics Playground app for all supported platforms',
+    () => {
 
       // Build the app for all supported platforms.
-      grunt.log.writeln( `${commandResult}` );
       grunt.log.writeln( 'Building app for all supported platforms...' );
-      commandResult = child_process.execSync( 'cordova build' );
+      const commandResult = child_process.execSync( 'cordova build' );
       grunt.log.writeln( `${commandResult}` );
     }
   );
 
   // register default task
-  grunt.registerTask( 'default', [ 'build' ] );
+  grunt.registerTask( 'default', [ 'lint', 'install-or-update-plugin', 'build' ] );
 };
