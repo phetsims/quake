48f359b124f0461ca0f1a8fa45188828a42dfd72
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-20 11:52:21 -0700

    add .js suffix back to require statements, https://github.com/phetsims/chipper/issues/1498

--- Gruntfile.cjs
+++ Gruntfile.cjs
@@ -1,7 +1,7 @@
 // Copyright 2020-2024, University of Colorado Boulder
 
 const Gruntfile = require( '../chipper/Gruntfile.cjs' );
-const registerTasks = require( '../perennial-alias/js/grunt/commonjs/registerTasks' );
+const registerTasks = require( '../perennial-alias/js/grunt/commonjs/registerTasks.js' );
 
 /**
  * quake-specific grunt configuration, builds the Haptics Playground app
