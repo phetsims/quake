4c30a5251c9de53a3f675899d62f9a47de33c3b9
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-12-03 14:16:55 -0700

    fix incorrect ignore for nested eslint configs, https://github.com/phetsims/chipper/issues/1483

--- js/grunt/eslint.config.mjs
+++ js/grunt/eslint.config.mjs
@@ -8,9 +8,10 @@
  */
 
 import nodeEslintConfig from '../../../perennial-alias/js/eslint/config/node.eslint.config.mjs';
+import { mutateForNestedConfig } from '../../../perennial-alias/js/eslint/config/root.eslint.config.mjs';
 
 export default [
-  ...nodeEslintConfig,
+  ...mutateForNestedConfig( nodeEslintConfig ),
   {
     // TODO: remove once the path works in root config again, https://github.com/phetsims/chipper/issues/1483/
     rules: {
